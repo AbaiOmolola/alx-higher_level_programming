69738590ab838226c95d9b94c6bd8e59a97eb41a
Abai Omolola <105018454+AbaiOmolola@users.noreply.github.com>
2024-01-04 23:09:28 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -3,7 +3,7 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         s = add(a, b)
-        for i in range(4, 7):
+        for i in range(4, 6):
             s = add(s, i)
         return s
     else: 
