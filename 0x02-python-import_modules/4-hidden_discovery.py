d876721d8eb11d704d1d8a84c0691207ef26def7
Abai Omolola <105018454+AbaiOmolola@users.noreply.github.com>
2024-01-05 00:07:45 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     name = dir(hidden_4)
     for i in range(len(name)):
-        if name[i][0] != "_" and name[1][i] != "_":
+        if name[i][0] != "_" and name[i][1] != "_":
             print(name[i])
