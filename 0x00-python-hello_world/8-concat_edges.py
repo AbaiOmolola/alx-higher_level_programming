9abe898844ec1f86766bb5a0c08408812e768268
AbaiOmolola <abaimaryomolola@gmail.com>
2024-01-04 07:41:03 -0800

    hygr

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/binn/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = ("\"object-oriented programmin with python") 
+print(str)
