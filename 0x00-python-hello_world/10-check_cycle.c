79f0c74f30cb0aaa6a2f21db5c77610b3dc7f088
AbaiOmolola <abaimaryomolola@gmail.com>
2024-01-04 16:33:28 -0800

    bvser

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1 = NULL;
+	listint_t *ptr2 = NULL;
+
+	if (list == NULL)
+		return (0);
+	ptr1 = list;
+	ptr2 = list;
+
+	while (ptr1 && ptr2 && ptr2->next)
+	{
+		ptr1 = ptr1->next;
+		ptr1 = ptr2->next->next;
+
+		if (ptr1 == ptr2)
+			return (1);
+	}
+	return (0);
+}
